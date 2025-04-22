bbf59e508b3c8adc2e022e2ff3c8d8c756a52ecd
ghostyghast <antoinethebestgamer@gmail.com>
2025-04-22 19:14:02 +0800

    feat: added extra test to show contructor exceptions

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 20:05:10 by amaligno          #+#    #+#             */
-/*   Updated: 2025/01/23 19:24:06 by amaligno         ###   ########.fr       */
+/*   Updated: 2025/04/22 19:11:28 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,29 @@
 
 int	main(void)
 {
-	Bureaucrat	guy("guy", 50);
+std::cout << "------Test1------\n";
+	try
+	{
+		Bureaucrat	guy("guy", 500);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Bureaucrat	guy("guy", -10);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+std::cout << "------Test2------\n";
+
 	Bureaucrat	bob;
-	
+	Bureaucrat	guy("guy", 50);
+
 	std::cout << bob << '\n';
 	try
 	{
